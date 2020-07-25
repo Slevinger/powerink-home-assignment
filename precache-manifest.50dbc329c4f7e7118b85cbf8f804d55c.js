e1ff61fa14f69233f1cdc1dec1a38fc1e2f7a2bf
slevinger <shir.levinger@gmail.com>
2020-07-25 20:48:04 +0300

    Updates

---
+++ precache-manifest.50dbc329c4f7e7118b85cbf8f804d55c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55292c8b70f47822834ea1ef654e45c",
+    "url": "/powerlink-home-assignment/index.html"
+  },
+  {
+    "revision": "ddf53971cb4bb31c96bf",
+    "url": "/powerlink-home-assignment/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "b207cc08dc3c8a8fc2d5",
+    "url": "/powerlink-home-assignment/static/css/main.3d479335.chunk.css"
+  },
+  {
+    "revision": "ddf53971cb4bb31c96bf",
+    "url": "/powerlink-home-assignment/static/js/2.17f14d46.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/powerlink-home-assignment/static/js/2.17f14d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b207cc08dc3c8a8fc2d5",
+    "url": "/powerlink-home-assignment/static/js/main.a27b3a36.chunk.js"
+  },
+  {
+    "revision": "018e5543ed4b410c19ac",
+    "url": "/powerlink-home-assignment/static/js/runtime-main.a08254e7.js"
+  }
+]);
